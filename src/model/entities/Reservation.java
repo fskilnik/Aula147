ce86c4a74050d9b0a01b29ca4622847d4f93741f
Fabio Skilnik <fskilnik@gmail.com>
2024-11-03 18:57:35 -0300

    Good solution (standard exceptions)

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,13 +5,13 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	 
+
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
@@ -36,36 +36,27 @@ public class Reservation {
 
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
-		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	
-	public String updateDates(Date checkIn, Date checkOut) {
-		this.checkIn = checkIn;
-		this.checkOut = checkOut;
-		
+
+	public void updateDates(Date checkIn, Date checkOut) {
+
 		Date now = new Date();
 
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "reservation dates must be future dates";
-		} 
+			throw new IllegalArgumentException("Reservation dates must be future dates");
+		}
 		if (!checkOut.after(checkIn)) {
-			return "checkOut before checkIn";
-		} 
-		return null;
+			throw new IllegalArgumentException("CheckOut must be after checkIn");
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room: "
-				+ roomNumber
-				+ " , check-in: "
-				+ sdf.format(checkIn)
-				+ " , check-out: "
-				+ sdf.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " nights";				
+		return "Room: " + roomNumber + " , check-in: " + sdf.format(checkIn) + " , check-out: " + sdf.format(checkOut)
+				+ ", " + duration() + " nights";
 	}
-	
 
 }
